173d207d05288a4e583abe0bb9cdb4cd3e141b7d
Shamsuddin Refaei <shamss.aldin@gmail.com>
2022-08-01 18:11:37 +0100

    Initial Commit

---
+++ Shared/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  Shared
+//
+//  Created by Shamsuddin Refaei on 01/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
